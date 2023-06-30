dbe4ecd48970bf49a51e7cc51ea3af3e6232a3a6
romandelcerro <romandelcerro.dev@gmail.com>
2023-06-30 15:29:27 +0200

    End App

---
+++ src/app/shared/components/interfaces/character.interface.ts
@@ -0,0 +1,13 @@
+export interface Character{
+    id: number;
+    name: string;
+    image: string;
+    species: string;
+    gender: string;
+    created: string;
+    status: string;
+    origin: {
+        name: string;
+        url: string;
+    };
+}
